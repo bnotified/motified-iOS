a246540eed63e7ceeb8b66456a9ad5b32f0d5590
Giancarlo Anemone <ganemone@gmail.com>
2015-03-17 13:19:08 -0400

    Implemented basics for creating new event

---
+++ MotifiedDateFormatter.swift
@@ -0,0 +1,13 @@
+//
+//  MotifiedDateFormatter.swift
+//  motified
+//
+//  Created by Giancarlo Anemone on 3/17/15.
+//  Copyright (c) 2015 Giancarlo Anemone. All rights reserved.
+//
+
+import UIKit
+
+class MotifiedDateFormatter: NSDateFormatter {
+   
+}
