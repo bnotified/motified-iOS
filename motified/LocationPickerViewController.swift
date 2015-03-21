0d0adac935034b006b395cd9c27b17c6d04a5e32
Giancarlo Anemone <ganemone@gmail.com>
2015-03-21 17:04:14 -0400

    Implement filtering of locations

--- motified/LocationPickerViewController.swift
+++ motified/LocationPickerViewController.swift
@@ -8,46 +8,81 @@
 
 import UIKit
 import MapKit
+import AddressBookUI
 
-class LocationPickerViewController: UIViewController, UISearchBarDelegate, UITableViewDelegate, UITableViewDataSource {
+func debounce( delay:NSTimeInterval, queue:dispatch_queue_t, action: (()->()) ) -> ()->() {
+    var lastFireTime:dispatch_time_t = 0
+    let dispatchDelay = Int64(delay * Double(NSEC_PER_SEC))
+    
+    return {
+        lastFireTime = dispatch_time(DISPATCH_TIME_NOW,0)
+        dispatch_after(
+            dispatch_time(
+                DISPATCH_TIME_NOW,
+                dispatchDelay
+            ),
+            queue) {
+                let now = dispatch_time(DISPATCH_TIME_NOW,0)
+                let when = dispatch_time(lastFireTime, dispatchDelay)
+                if now >= when {
+                    action()
+                }
+        }
+    }
+}
+
+class LocationPickerViewController: UIViewController, UISearchBarDelegate, UITableViewDelegate, UITableViewDataSource, LocationPickerViewDelegate {
 
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var locationPickerView: LocationPickerView!
     
-    let defaultLocations: Array<Dictionary<String, AnyObject!>> = [
+    var defaultLocations: Array<Dictionary<String, AnyObject!>> = [
         [
-            "title": "The Neutral Zone, Ann Arbor, Michigan",
+            "title": "The Neutral Zone",
+            "right": "",
             "annotation": nil
         ],
         [
-            "title": "Zingerman's Bakehouse, Ann Arbor, Michigan",
+            "title": "Zingerman's Bakehouse",
+            "right": "",
             "annotation": nil
         ],
         [
-            "title": "The Michigan Theater, Ann Arbor, Michigan",
+            "title": "The Michigan Theater",
+            "right": "",
             "annotation": nil
         ],
         [
-            "title": "The Yellow Barn, Ann Arbor, Michigan",
+            "title": "The Yellow Barn",
+            "right": "",
             "annotation": nil
         ],
         [
-            "title": "Washtenaw Community College, Ann Arbor, Michigan",
+            "title": "Washtenaw Community College",
+            "right": "",
             "annotation": nil
         ]
     ]
     
     var searchResults: Array<Dictionary<String, AnyObject!>> = []
+    var selectedLocation: Dictionary<String, AnyObject!>! = nil
+    var debouncedSearch: (()->())? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Set up delegates
+        self.locationPickerView.delegate = self
         self.locationPickerView.tableViewDataSource = self
         self.locationPickerView.tableViewDelegate = self
         self.locationPickerView.defaultMapHeight = self.locationPickerView.defaultMapHeight + 20
         
+        self.searchBar.delegate = self
+        
         // Optional setup
         self.locationPickerView.shouldCreateHideMapButton = true
+        
+        self.debouncedSearch = debounce(NSTimeInterval(0.25), dispatch_get_main_queue(), self.makeRequest)
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -60,8 +95,63 @@ class LocationPickerViewController: UIViewController, UISearchBarDelegate, UITab
     }
     
     func makeRequest() {
+        if self.searchBar.text.utf16Count == 0 {
+            return ()
+        }
         let request = MKLocalSearchRequest()
-        request.naturalLanguageQuery = searchBar.text
+        request.naturalLanguageQuery = self.searchBar.text + "Ann Arbor, Michigan, United States"
+        let map = self.locationPickerView.mapView
+        request.region = map.region
+        
+        let localSearch = MKLocalSearch(request: request)
+        localSearch.startWithCompletionHandler { (MKLocalSearchResponse, NSError) -> Void in
+            if NSError != nil {
+                NSLog("Error: %@", NSError)
+                return ()
+            }
+            if MKLocalSearchResponse.mapItems.count == 0 {
+                self.view.makeToast("No results")
+                return ()
+            }
+            let items = MKLocalSearchResponse.mapItems as [MKMapItem]
+            self.clearAnnotations()
+            self.updateSearchResults(items)
+            self.updateAnnotations(items)
+        }
+    }
+    
+    func centerOnAnnArbor() {
+        let request = MKLocalSearchRequest()
+        request.naturalLanguageQuery = "Ann Arbor Michigan"
+        let map = self.locationPickerView.mapView
+        request.region = map.region
+        let localSearch = MKLocalSearch(request: request)
+        localSearch.startWithCompletionHandler { (MKLocalSearchResponse, NSError) -> Void in
+            if NSError != nil {
+                NSLog("Error: %@", NSError)
+                return ()
+            }
+            if MKLocalSearchResponse.mapItems.count == 0 {
+                return ()
+            }
+            let items = MKLocalSearchResponse.mapItems as [MKMapItem]
+            let placemark = items.first!.placemark
+            map.showAnnotations([placemark], animated: false)
+            map.region.span.longitudeDelta = 0.05
+            map.region.span.latitudeDelta = 0.05
+            map.removeAnnotation(placemark)
+        }
+    }
+    
+    internal func loadDefaultPlacemarks() {
+        for x in 0...self.defaultLocations.count - 1 {
+            self.loadPlacemarkForDefaultAtIndex(x)
+        }
+    }
+    
+    internal func loadPlacemarkForDefaultAtIndex(index: Int) {
+        let request = MKLocalSearchRequest()
+        request.naturalLanguageQuery = self.defaultLocations[index]["title"]! as String + " Ann Arbor, Michigan"
         let map = self.locationPickerView.mapView
         request.region = map.region
         
@@ -76,16 +166,44 @@ class LocationPickerViewController: UIViewController, UISearchBarDelegate, UITab
                 return ()
             }
             let items = MKLocalSearchResponse.mapItems as [MKMapItem]
-            self.updateSearchResults(items)
-            self.updateAnnotations(items)
+            let placemark = items.first!.placemark as MKPlacemark
+            let address = self.getFormattedAddress(items.first!)
+            self.defaultLocations[index].updateValue(placemark, forKey: "annotation")
+            self.defaultLocations[index].updateValue(address, forKey: "right")
+            
         }
+    }
+    
+    internal func getFormattedAddress(item: MKMapItem) -> String {
+        let placemark = item.placemark
+        let number = placemark.subThoroughfare ?? ""
+        let address = placemark.thoroughfare ?? ""
+        let postal = placemark.postalCode ?? ""
+        let locality = placemark.locality ?? ""
+        
+        return "\(number) \(address)\n\(postal) \(locality)"
+    }
+    
+    internal func getName(item: MKMapItem) -> String {
+        let placemark = item.placemark
+        if let street: AnyObject = placemark.addressDictionary["Street"] {
+            if let st = street as? String {
+                if placemark.name == st {
+                    return ""
+                }
+            }
+        }
+        return placemark.name
     }
     
     func updateSearchResults(mapItems: Array<MKMapItem>) {
         self.searchResults.removeAll(keepCapacity: true)
         for item in mapItems {
+            let title = self.getFormattedAddress(item)
+            let right = self.getName(item)
             self.searchResults.append([
-                "title": item.name,
+                "title": title,
+                "right": right,
                 "annotation": item.placemark
             ])
         }
@@ -109,10 +227,24 @@ class LocationPickerViewController: UIViewController, UISearchBarDelegate, UITab
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let identifier = "location_cell_identifier"
         let data = self.getDataForRow(indexPath.row)
+        let cell = UITableViewCell(style: UITableViewCellStyle.Value1, reuseIdentifier: identifier)
         
         let title = data["title"]! as String
-        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: identifier)
+        if let right: AnyObject! = data["right"] {
+            if let rt = right as? String {
+                cell.detailTextLabel?.text = rt
+            }
+        }
+
+        cell.textLabel?.numberOfLines = 0
         cell.textLabel?.text = title
+        
+        cell.detailTextLabel?.numberOfLines = 0
+
+        
+        
+        cell.textLabel?.sizeToFit()
+        cell.detailTextLabel?.sizeToFit()
         
         return cell
     }
@@ -130,6 +262,25 @@ class LocationPickerViewController: UIViewController, UISearchBarDelegate, UITab
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        let label = UILabel()
+        let data = self.getDataForRow(indexPath.row)
+        label.numberOfLines = 0
+        if let right: AnyObject! = data["right"] {
+            if let rt = right as? String {
+                label.text = rt
+            } else {
+                return 50
+            }
+        } else {
+            return 50
+        }
+        let constraints = CGSizeMake(self.view.frame.width / 2, CGFloat.max)
+        let sizeToFit2 = label.sizeThatFits(constraints)
+        
+        return max(50, sizeToFit2.height + 20)
     }
     
     func getDataForRow(row: Int) -> Dictionary<String, AnyObject!> {
@@ -154,14 +305,40 @@ class LocationPickerViewController: UIViewController, UISearchBarDelegate, UITab
             map.addAnnotation(annotation)
             self.showAnnotations()
         }
+        self.selectedLocation = data
+        self.updateBackButton()
+    }
+    
+    internal func updateBackButton() {
+        if self.selectedLocation != nil {
+            self.navigationItem.backBarButtonItem?.title = "Done"
+        } else {
+            self.navigationItem.backBarButtonItem?.title = "Cancel"
+        }
     }
     
     internal func showAnnotations() {
         let map = self.locationPickerView.mapView
-        map.showAnnotations(map.annotations, animated: true)
+        map.showAnnotations(map.annotations, animated: false)
+        let center = map.centerCoordinate
+        let newCenter: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: center.latitude - map.region.span.latitudeDelta * 0.20, longitude: center.longitude)
+        map.setCenterCoordinate(newCenter, animated: true)
+    }
+    
+    func myMKMapRect(x: Double, y:Double, w:Double, h:Double) -> MKMapRect {
+        return MKMapRect(origin:MKMapPoint(x:x, y:y), size:MKMapSize(width:w, height:h))
     }
     
     internal func clearAnnotations() {
         self.locationPickerView.mapView.removeAnnotations(self.locationPickerView.mapView.annotations)
+    }
+    
+    func locationPicker(locationPicker: LocationPickerView!, mapViewDidLoad mapView: MKMapView!) {
+        self.loadDefaultPlacemarks()
+        self.centerOnAnnArbor()
+    }
+    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        self.debouncedSearch!()
     }
 }
