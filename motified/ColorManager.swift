3dfc65c74c6a3535b6e1ed79107bb1ac7923c26b
Giancarlo Anemone <ganemone@gmail.com>
2015-03-25 17:03:23 -0400

    Add color manager

---
+++ motified/ColorManager.swift
@@ -0,0 +1,13 @@
+//
+//  ColorManager.swift
+//  motified
+//
+//  Created by Giancarlo Anemone on 3/25/15.
+//  Copyright (c) 2015 Giancarlo Anemone. All rights reserved.
+//
+
+import UIKit
+
+class ColorManager: NSObject {
+   
+}
