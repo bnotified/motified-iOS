be3b912db44158e0848213bb64dc61975e0ff1f0
Giancarlo Anemone <ganemone@gmail.com>
2015-03-27 12:53:13 -0400

    Added helper function for adding borders to buttons

---
+++ motified/addBorder.swift
@@ -0,0 +1,16 @@
+//
+//  addBorder.swift
+//  motified
+//
+//  Created by Giancarlo Anemone on 3/27/15.
+//  Copyright (c) 2015 Giancarlo Anemone. All rights reserved.
+//
+
+import Foundation
+
+func addBorder(button: UIButton) -> Void {
+    button.layer.borderWidth = 1.0
+    button.layer.cornerRadius = 8.0
+    button.layer.borderColor = UIColor.whiteColor().CGColor
+    return ()
+}
